97fbc58edeff840aed211361b6d351c0f3bbf307
shiotomo <t.shiota16@gmail.com>
2018-06-06 00:41:58 +0900

    first commit

---
+++ js/scroll.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('a[href^="#"]').click(function(){
+    const speed = 500;
+    const href = $(this).attr("href");
+    const target = $(href == "#" || href == "" ? 'html' : href);
+    const position = target.offset().top;
+
+    $('html, body').animate({scrollTop: position}, speed, 'swing');
+
+    return false;
+  });
+});
